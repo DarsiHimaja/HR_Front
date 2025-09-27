e0815101d450c91d6acd04b242d138e57aaf2531
DarsiHimaja <himajah99@gmail.com>
2025-09-27 12:33:25 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+
 const api = axios.create({
-  baseURL:"backend2-1-labd.onrender.com",
+  baseURL: "https://backend2-1-labd.onrender.com", // added https://
 });
+
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem("token");
-  if(token) cfg.headers.Authorization = `Bearer ${token}`;
+  if (token) cfg.headers.Authorization = `Bearer ${token}`;
   return cfg;
 });
+
 export default api;
